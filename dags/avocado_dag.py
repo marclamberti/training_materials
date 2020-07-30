bc4ae3cb051bed20be52d29a2094f69b16021448
Marc Lamberti <marclamberti@pc-79.home>
2020-07-30 17:33:36 +0200

    update

--- dags/avocado_dag.py
+++ dags/avocado_dag.py
@@ -1,12 +1,18 @@
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
-import wget
+import urllib.request
 from datetime import datetime
 
 def download_dataset():
 	url = 'https://raw.githubusercontent.com/marclamberti/training_materials/master/data/avocado.csv'
-	wget.download(url, out='/usr/local/airflow/avocado.csv')
+	urllib.request.urlretrieve(url, filename='/tmp/avocado.csv')
+
+def read_rmse():
+    accuracy = 0
+    with open('/tmp/out-model-avocado-prediction-rmse.txt') as f:
+        accuracy = float(f.readline())
+    return 'accurate' if accuracy < 0.15 else 'inaccurate'
 
 dag = DAG('avocado_dag', description='Forecasting avocado prices', schedule_interval='*/10 * * * *', start_date=datetime(2020, 1, 1), catchup=False)
 
